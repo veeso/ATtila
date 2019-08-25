c665b55d3e221fd26c4faa19e7954594f68c21bd
ChristianVisintin <christian.visintin1997@gmail.com>
2019-08-25 12:08:14 +0200

    ATSession

--- attila/atsession.py
+++ attila/atsession.py
@@ -19,3 +19,225 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 #
+
+from .exceptions import ATSerialPortError
+from .atcommand import ATCommand
+from .atresponse import ATResponse
+
+from re import sub, search, escape
+
+class ATSession(object):
+  """
+  This class represents an AT sessions, which is a set of commands to execute - "a script".
+  It takes care of preparing the next command to execute based on the response of the previous one
+  and of validating the response of the last command. 
+  """
+
+  def __init__(self, commands):
+    """
+    Class constructor. Instantiates a new :class:`.ATSession.` object with the provided parameters.
+
+    :param commands: list of ATCommand which will become the set of instructions for this session.
+    :type commands: list
+    """
+    self._commands = commands
+    self._session_values = {}
+    self._current_command = 0
+    self._last_command_failed = False
+
+  def reset(self):
+    """
+    Reset the AT session. It clears the command list and the session values
+    """
+    self._commands = []
+    self._session_values = {}
+    self._current_command = 0
+    self._last_command_failed = False
+  
+  def add_command(self, command):
+    """
+    Add a command at the end of the command list
+
+    :param command: command to add
+    :type command: ATCommand
+    :returns boolean
+    """
+    self._commands.append(command)
+    return True
+
+  def add_new_command(self, command, exp_response, tout = 10, delay = 0, collectables = None, dganger = None):
+    """
+    Add a new command at the end of the command list
+
+    :param command: command to add
+    :param exp_response: expected response from command execution. a literal or a generic response can be provided
+    :param tout (optional): command timeout execution.
+    :param delay (optional): delay in milliseconds before command execution
+    :param collectables (optional): values to store from response. Follow collectables syntax as specified in ATtila documentation
+    :param dganger (optional): doppelganger command associated to this command (command to execute in case of this command fails)
+    :type command: string
+    :type ATResponse: string
+    :type exp_respose: string
+    :type tout: int
+    :type delay: int
+    :type collectables: list of string
+    :type dganger: ATCommand
+    :returns boolean
+    """
+    new_command = ATCommand(command, exp_response, tout, delay, collectables, dganger)
+    if not new_command:
+      return False
+    self._commands.append(new_command)
+    return True
+
+  def rem_command(self, index):
+    """
+    Remove a command from the session command list
+
+    :param index: index of the command to remove
+    :type index: int
+    :returns boolean
+    """
+    if index >= len(self._commands):
+      return False
+    self._commands.remove(self._commands[index])
+    return True
+
+  def get_next_command(self):
+    """
+    Get the next command in the AT session to execute
+    
+    :returns ATCommand
+    """
+    #Prepare command
+    self.prepare()
+    #Return command
+    return self._commands.at(self._current_command)
+
+  def get_command(self, index):
+    """
+    Get the command with the provided index
+    
+    :param index: index of the command to get
+    :type index: int
+    :returns ATCommand
+    """
+    if index >= len(self._commands):
+      return None
+    return self._commands.at(index)
+
+  def validate_response(self, response, execution_time):
+    """
+    Validate a response of a command. The response is associated to the current command.
+    Based on the response and the command associated to it,
+    the ATResponse will have collectables based on expected response,
+    also the next command, in case of a wrong response will be the doppelganger (if set)
+    :param response
+    :param execution_time
+    :type response: list of string
+    :type execution_time: int
+    :returns ATResponse
+    """
+    #Get current command expected response
+    current_command = self._commands.at(self._current_command)
+    #Increment current command
+    self._current_command += 1
+    #Prepare variables for looking for response
+    expected_response = current_command.expected_response
+    vars_to_collect = current_command.collectables
+    #Variables for response object
+    response_str = None
+    #Search for response
+    for line in response:
+      #Search for expected response in line
+      if search(expected_response, line):
+        response_str = line
+        self._last_command_failed = False
+        break
+    #If response hasn't been found => last command failed 
+    if not response_str:
+      self._last_command_failed = True
+    #Instance ATResponse
+    atresponse = ATResponse(response_str, response, execution_time)
+    #If last command failed => set doppelganger as last command
+    if self._last_command_failed:
+      #@! Response NOK
+      doppelganger = current_command.doppel_ganger
+      if doppelganger:
+        #Add command to command list
+        self._commands.insert(doppelganger, self._current_command)
+    else:
+      #@! Response OK
+      #Try to get collectables
+      for to_collect in vars_to_collect: #String
+        collected = self.__get_value_from_response(to_collect, response) #collected => tuple(key, value)
+        if collected:
+          self._session_values[collected[0]] = collected[1]
+          atresponse.add_collectable(collected[0], collected[1])
+    #Instance response object
+    current_command.response = atresponse
+    return atresponse
+
+  def __get_value_from_response(self, to_collect, response):
+    """
+    Get a value from response.
+    The collectable syntax is 'XXXXXXXX?{KEY_NAME}XXXXXX'
+    The ?{} part is replaced by (.*) in a regex
+
+    :param to_collect: collectable syntax to match
+    :param response: list of string gained in the response
+    :type to_collect: string
+    :type response: list of string
+    :returns tuple(string, string/int); None if not found
+    """
+    reg_result = search("\\?{.*)", to_collect)
+    if reg_result:
+      key_group = reg_result.group()
+    else:
+      return None
+    key_name = key_group[2:-1]
+    part_to_remove = to_collect.replace(key_group, "")
+    key_value = None
+    regex = to_collect.replace(key_group, "")
+    #Escape regex
+    regex = "%s%s" % (escape(regex), "(.*)")
+    #Iterate over lines
+    for line in response:
+      if search(regex, line):
+        key_value = regex.replace(part_to_remove, "")
+        try:
+          key_value = int(key_value)
+        except ValueError:
+          pass
+        break
+    #Return values
+    if key_value:
+      return (key_name, key_value)
+    else:
+      return None
+
+  def prepare(self):
+    """
+    Prepare command to execute, replacing session variable with values in session;
+    if value is not in session, it will be replaced with an empty string
+    """
+    
+    current_command = self._commands.at(self._current_command)
+    command_str = current_command.command
+    #Get session variable
+    while search("\\${.*)", command_str):
+      reg_result = search("\\${.*)", command_str)
+      key_group = reg_result.group()
+      key_name = key_group[2:-1]
+      #@! Okay, there is a session variable to replace
+      #Search for session variable
+      session_value = self._session_values.get(key_name)
+      if not session_value:
+        session_value = ""
+      #Replace session variable
+      command_str = command_str.replace(key_group, session_value)
+    #@!All sessions variables have been replaced
+    #Other stuff???
+    #Reassing command to ATCommand
+    self._commands.at(self._current_command).command = command_str
+    return
