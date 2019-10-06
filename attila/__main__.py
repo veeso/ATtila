3fe25911f227e1a7769510292ddb5e3f7226de27
ChristianVisintin <christian.visintin1997@gmail.com>
2019-10-06 22:48:26 +0200

    ATtila main application; ready for live test session

--- attila/__main__.py
+++ attila/__main__.py
@@ -20,4 +20,268 @@
 # SOFTWARE.
 #
 
+PROGRAM_NAME = "attila"
 
+#Import ATtila
+from attila.atre import ATRuntimeEnvironment
+from attila.exceptions import ATREUninitializedError, ATRuntimeError, ATScriptNotFound, ATScriptSyntaxError, ATSerialPortError
+#System imports
+from sys import argv, exit, stdout
+#Logging
+import logging
+#Getopt
+from getopt import getopt, GetoptError
+#Signals
+from signal import signal, SIGTERM, SIGINT
+
+USAGE = "Usage: %s [OPTION]...\n\
+  \n\
+  With no FILE, run in interactive mode\n\
+  \n\
+  \t-f <atscript file>\tRun attila reading script from specified file\n\
+  \t-i\t\t\tRun attila in interactive mode (default)\n\
+  \t-p <device path>\tUse this device to communicate\n\
+  \t-b <baud rate>\t\tUse the specified baudrate to communicate\n\
+  \t-T <default timeout>\tUse the specified timeout as default to communicate\n\
+  \t-B <break>\t\tUse the specified line break [CRLF, LF, CR] (Default: CRLF)\n\
+  \t-A <True/False>\t\t\tAbort on failure (Default: True)\n\
+  \t-L <logfile>\t\tEnable log and log to the specified log file (stdout is supported)\n\
+  \t-l <loglevel>\t\tSpecify the log level (0: CRITICAL, 1: ERROR, 2: WARN, 3: INFO, 4: DEBUG) (Default: INFO\n\
+  \t-v\t\t\tBe verbose\n\
+  \t-h\t\t\tShow this page\n\
+  " % PROGRAM_NAME
+
+LOG_LEVEL_DEBUG = 4
+LOG_LEVEL_INFO = 3
+LOG_LEVEL_WARN = 2
+LOG_LEVEL_ERROR = 1
+LOG_LEVEL_CRITICAL = 0
+
+#Globals
+sigterm_called = False
+
+def sigterm_handler(_signo, _stack_frame):
+  """
+  Handle sigterm (or sigint) setting sigterm_called to True
+  """
+  global sigterm_called
+  logging.warn("SIGTERM called")
+  sigterm_called = True
+
+def opt_error(message):
+  """
+  Function to call in case of an error while parsing options and terminates with exit code 1
+
+  :param message
+  :type message: String
+  """
+  print(message)
+  print(USAGE)
+  exit(1)
+
+def get_log_level_from_option(log_level_int):
+  """
+  Get log level from attila option
+
+  :param log_level_num
+  :type int
+  :returns logging level
+  """
+  if log_level_int == LOG_LEVEL_DEBUG:
+    return logging.DEBUG
+  elif log_level_int == LOG_LEVEL_INFO:
+    return logging.INFO
+  elif log_level_int == LOG_LEVEL_WARN:
+    return logging.WARNING
+  elif log_level_int == LOG_LEVEL_ERROR:
+    return logging.ERROR
+  elif log_level_int == LOG_LEVEL_CRITICAL:
+    return logging.CRITICAL
+  else:
+    return logging.INFO
+
+
+if __name__ == "__main__":
+  #Options
+  interactive_mode = True
+  script_file = None
+  device = None
+  baud_rate = None
+  default_timeout = None
+  line_break = None
+  logfile = None
+  log_level = LOG_LEVEL_INFO
+  verbose = False
+  abort_on_failure = True
+  to_stdout = False
+
+  try:
+    optlist, args = getopt(argv[1:], "f::ip::b::T::B::L::l::Avh")
+    for opt, arg in optlist:
+      if opt == "-f":
+        interactive_mode = False
+        script_file = arg
+      elif opt == "-i":
+        interactive_mode = True
+        script_file = None
+      elif opt == "-p":
+        device = arg
+      elif opt == "-b":
+        try:
+          baud_rate = int(arg)
+        except ValueError:
+          opt_error("Specified baud rate is not a number!")
+      elif opt == "-T":
+        try:
+          default_timeout = int(arg)
+        except ValueError:
+          opt_error("Specified default timeout is not a number!")
+      elif opt == "-B":
+        line_break = arg
+        #Verify line break
+        if line_break != "CRLF" and line_break != "LF" and line_break != "CR":
+          opt_error("Invalid line break '%s'" % line_break)
+        else:
+          if line_break == "CRLF":
+            line_break = "\r\n"
+          elif line_break == "LF":
+            line_break = "\n"
+          elif line_break == "CR":
+            line_break = "\r"
+      elif opt == "-L":
+        logfile = arg
+      elif opt == "-l":
+        try:
+          log_level = int(arg)
+          if log_level < LOG_LEVEL_CRITICAL or log_level > LOG_LEVEL_DEBUG:
+            opt_error("Log level is out of range")
+          else:
+            log_level = get_log_level_from_option(log_level)
+        except ValueError:
+          opt_error("Log level is not a number")
+      elif opt == "-A":
+        try:
+          abort_on_failure = eval(arg)
+          if abort_on_failure != True and abort_on_failure != False:
+            opt_error("Abort on failure has a bad value: '%s', but should be True or False" % abort_on_failure)
+        except NameError:
+          opt_error("Abort on failure has a bad value")
+      elif opt == "-v":
+        verbose = True
+      elif opt == "-h":
+        print(USAGE)
+        exit(0)
+      else:
+        opt_error("Unkown option '%s'" % opt)
+  except GetoptError as err:
+    opt_error(err)
+
+  #Set signal handlers listener
+  signal(SIGTERM, sigterm_handler)
+  signal(SIGINT, sigterm_handler)
+
+  #Prepare logger if requested
+  if logfile:
+    if logfile == "stdout":
+      to_stdout = True
+      stdout_handler = logging.StreamHandler(stdout)
+      stdout_handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s]: %(message)s", datefmt="%Y-%m-%dT%H:%M:%S"))
+      logging.getLogger().addHandler(stdout_handler)
+    else:
+      logging.basicConfig(filename=logfile, level=log_level, format="%(asctime)s [%(levelname)s]: %(message)s", datefmt="%Y-%m-%dT%H:%M:%S")
+  #Instance ATRuntime environment
+  atrunenv = ATRuntimeEnvironment(abort_on_failure)
+  #Configure serial
+  if device and baud_rate:
+    atrunenv.configure_communicator(device, baud_rate, default_timeout, line_break)
+    logging.info("Setup communicator (device: %s, baud_rate: %d)" % (device, baud_rate))
+    if verbose and not to_stdout:
+      print("Setup communicator (device: %s, baud_rate: %d)" % (device, baud_rate))
+  #Open serial
+  try:
+    atrunenv.open_serial()
+  except ATSerialPortError as err:
+    logging.error("Could not open serial port: %s" % err)
+    if not to_stdout:
+      print("Could not open serial port: %s" % err)
+    exit(1)
+  #Parse file if set
+  if script_file:
+    logging.debug("Trying to parse file %s..." % script_file)
+    try:
+      atrunenv.parse_ATScript(script_file)
+    except ATScriptNotFound as err:
+      logging.error("Could not find script file %s: %s" % (script_file, err))
+      if not to_stdout:
+        print("Could not find script file %s: %s" % (script_file, err))
+      exit(1)
+    except ATScriptSyntaxError as err:
+      logging.error("Script Syntax error: %s" % err)
+      exit(1)
+    #Execute script
+    response = 1
+    while response and not sigterm_called:
+      try:
+        response = atrunenv.exec_next()
+        #Handle response
+        if response.response and response.command:
+          logging.info("%s (%d ms) >> %s" % (response.command.command, response.execution_time, response.response))
+        else: #Command failed (this snippet gets executed only if aof is false)
+          logging.error("%s (%d ms) >> %s" % (response.command.command, response.execution_time, "\n".join(response.full_response)))
+      except ATSerialPortError as err:
+        logging.error("Serial Port error: %s" % err)
+        if not to_stdout:
+          print("Serial Port error: %s" % err)
+        if atrunenv.aof:
+          break
+      except ATRuntimeError as err:
+        logging.error("Runtime error: %s" % err)
+        if not to_stdout:
+          print("Runtime error: %s" % err)
+        if atrunenv.aof:
+          break
+  else: #Interactive mode
+    while not sigterm_called:
+      command_line = input(">> ")
+      #Parse and execute command
+      try:
+        response = atrunenv.exec(command_line)
+        if response: #Was a command (otherwise was probably ESK)
+          if response.response: #Command was successful
+            logging.info("%s (%d ms) >> %s" % (response.command.command, response.execution_time, response.response))
+            if not to_stdout:
+              print("%s (%d ms) >> %s" % (response.command.command, response.execution_time, response.response))
+          else: #Command error
+            logging.error("%s (%d ms) >> %s" % (response.command.command, response.execution_time, "\n".join(response.full_response)))
+            if not to_stdout:
+              print("%s (%d ms) >> %s" % (response.command.command, response.execution_time, "\n".join(response.full_response)))
+        else:
+          logging.info("%s >> OK" % command_line)
+          if not to_stdout:
+            print("%s >> OK" % command_line)
+      except ATScriptSyntaxError as err:
+        logging.error("Syntax error: %s" % err)
+        if not to_stdout:
+          print("Syntax error: %s" % err)
+        continue
+      except ATSerialPortError as err:
+        logging.error("Serial Port error: %s" % err)
+        if not to_stdout:
+          print("Serial Port error: %s" % err)
+      except ATRuntimeError as err:
+        logging.error("Runtime error: %s" % err)
+        if not to_stdout:
+          print("Runtime error: %s" % err)
+  #Close serial
+  try:
+    atrunenv.close_serial()
+  except ATSerialPortError as err:
+    logging.error("Could not close serial port: %s" % err)
+    if not to_stdout:
+      print("Could not close serial port: %s" % err)
+    exit(1)
+  #Execution terminated
+  logging.info("attila terminated with exit code 0")
+  if not to_stdout and verbose:
+    print("attila terminated with exit code 0")
+  exit(0)
