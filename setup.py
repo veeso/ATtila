a949a1d53b9cbdcdcb4ab8a9a961d098988f1467
veeso <christian.visintin1997@gmail.com>
2022-07-29 11:48:19 +0200

    fixed descr

--- setup.py
+++ setup.py
@@ -1,50 +1,48 @@
 #!/usr/bin/python3
 
-from setuptools import find_packages, setup, Extension
+from setuptools import find_packages, setup
 from pathlib import Path
-from os import path
 
 # The directory containing this file
 ROOT = Path(__file__).parent
 
-README = (ROOT / "README.md").read_text(encoding="utf-8")
+README = (ROOT / "README.pypi.md").read_text(encoding="utf-8")
 
 setup(
-    name='attila',
-    version='1.2.1',
-    description='Python module to communicate easily with modems and RF modules using AT commands',
+    name="attila",
+    version="1.2.1",
+    description="Python module to communicate easily with modems and RF modules using AT commands",
     long_description=README,
     long_description_content_type="text/markdown",
-    author='Christian Visintin',
-    author_email='christian.visintin1997@gmail.com',
-    url='https://github.com/veeso/ATtila',
+    author="Christian Visintin",
+    author_email="christian.visintin1997@gmail.com",
+    url="https://github.com/veeso/ATtila",
     license="MIT",
-    python_requires='>=3.5',
+    python_requires=">=3.5",
     include_package_data=True,
-    install_requires=[
-        'pyserial>=3',
-        'pathlib2>=2'
-    ],
-    entry_points={
-        'console_scripts': [
-            'attila = attila.__main__:main'
-        ]
-    },
+    install_requires=["pyserial>=3", "pathlib2>=2"],
+    entry_points={"console_scripts": ["attila = attila.__main__:main"]},
     packages=find_packages(exclude=("tests",)),
-    keywords=['AT commands', 'IOT', 'wireless',
-              'radio frequency', 'modem', 'rf modules'],
+    keywords=[
+        "AT commands",
+        "IOT",
+        "wireless",
+        "radio frequency",
+        "modem",
+        "rf modules",
+    ],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Telecommunications Industry',
-        'Intended Audience :: End Users/Desktop',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Home Automation',
-        'Topic :: Communications :: Telephony',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Telecommunications Industry",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Home Automation",
+        "Topic :: Communications :: Telephony",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
-    ]
+    ],
 )
